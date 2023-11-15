8e23358e7fdff32d17ece8a5605db4b57c5a7b1d
Maxim Slobodchikov <maxims@bu.edu>
2023-11-15 00:50:09 -0500

    update package.json

---
+++ src/components/Main.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './styles.css';
+import image1 from './image1.png';
+import image2 from './image2.png';
+import cat from './cat.png';
+import bunny from './bunny.png';
+
+
+const Main = () => {
+  return (
+    <div className="container">
+        {/* <div className="Img">
+            <img src={cat} alt="photo" width="100" height="100"/>
+        </div> */}
+
+      <header className="header">
+        Hi, my name is Maxim!
+      </header>
+
+      <div className="about">
+        <p>I'm a junior at Boston University studying Computer Science, where I teach accessibility and algorithmic bias.</p>
+      </div>
+
+      <ul className="links">
+        <li><a href="https://www.linkedin.com/in/maximslo/">LinkedIn</a></li>
+        <li><a href="https://github.com/maximslo">GitHub</a></li>
+        <li><a href="../public/MaximResume.pdf" target="_blank" rel="noopener noreferrer" aria-label="Open Resume PDF in a new tab" className="resume-link">Resume</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Main;
