8a87ad10424409782e84bfd8625eb7425de6fe31
Maxim Slobodchikov <mslobodchikov@wayfair.com>
2024-11-14 17:51:19 -0500

    senior year changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,17 +15,19 @@ const Main = () => {
         </div>
 
       <header className="header">
-        Hi, my name is Maxim!
+        Maxim Slobodchikov
       </header>
 
       <div className="about">
-        <p>I'm a junior at Boston University studying Computer Science, where I teach accessibility and algorithmic bias.</p>
+        <p>I'm a senior at Boston University studying Computer Science, where I teach accessibility and algorithmic bias.</p>
       </div>
 
       <ul className="links">
         <li><a href="https://www.linkedin.com/in/maximslo/">LinkedIn</a></li>
         <li><a href="https://github.com/maximslo">GitHub</a></li>
-        <li><a href="./MaximResume.pdf" target="_blank" rel="noopener noreferrer" aria-label="Open Resume PDF in a new tab" className="resume-link">Resume</a></li>
+        {/* <li><a href="./MaximResume.pdf" target="_blank" rel="noopener noreferrer" aria-label="Open Resume PDF in a new tab" className="resume-link">Resume</a></li> */}
+        <li><a href="https://strava.app.link/iaJoaMXRwOb">Strava</a></li>
+
       </ul>
     </div>
   );
