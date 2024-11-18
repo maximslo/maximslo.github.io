cb7845c7e789f3a51138873b31c0c4c265d66318
Maxim Slobodchikov <mslobodchikov@wayfair.com>
2024-11-18 04:47:22 -0500

    mobile friendly, left aligning

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './styles.css';
-import image1 from './image1.png';
-import image2 from './image2.png';
-import cat from './cat.png';
-import bunny from './bunny.png';
 import seed from './seedling.png';
-
 
 const Main = () => {
   return (
     <div className="container">
         <div className="Img">
-            <img src={seed} alt="photo" width="100" height="100"/>
+            <img src={seed} alt="photo" width="50" height="50"/>
         </div>
 
       <header className="header">
-        Maxim Slobodchikov
+        Maxim<br />
+        Slobodchikov
       </header>
 
       <div className="about">
@@ -25,9 +21,7 @@ const Main = () => {
       <ul className="links">
         <li><a href="https://www.linkedin.com/in/maximslo/">LinkedIn</a></li>
         <li><a href="https://github.com/maximslo">GitHub</a></li>
-        {/* <li><a href="./MaximResume.pdf" target="_blank" rel="noopener noreferrer" aria-label="Open Resume PDF in a new tab" className="resume-link">Resume</a></li> */}
         <li><a href="https://strava.app.link/iaJoaMXRwOb">Strava</a></li>
-
       </ul>
     </div>
   );
